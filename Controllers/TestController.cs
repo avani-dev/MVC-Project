fd26a7f581ca7f530caa061eada998aaa836342a
Avani Panchal <avanip.net@gmail.com>
2020-04-12 22:57:19 -0500

    MVC Sample

---
+++ Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public ViewResult Index_Test()
+        {
+            return View();
+        }
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ContentResult Index1()
+        {
+            return Content("<h3>Zain Ul Hassan </h3>");
+        }
+
+    }
+}
