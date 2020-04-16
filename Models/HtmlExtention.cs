f6577332b5e4652aba512976671b2de808f9f88f
Avani Panchal <avanip.net@gmail.com>
2020-04-15 22:10:56 -0500

    Initial Commit

---
+++ Models/HtmlExtention.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project.Models
+{
+    public static class HtmlExtention
+    {
+        public static MvcHtmlString SubmitButton(this HtmlHelper helper, String buttonText)
+        {
+            String str = "<input type=\"Submit\" Value=\"" + buttonText + "\" />";
+            return new MvcHtmlString(str);
+        }
+
+    }
+}
